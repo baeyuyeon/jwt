52ca200f87c714235ae71b3671f169204f0888ed
baeyuyeon <31380511+baeyuyeon@users.noreply.github.com>
2023-08-13 15:35:48 +0900

    JWT 연동완료

--- src/main/java/com/yuyeon/jwt/config/SecurityConfig.java
+++ src/main/java/com/yuyeon/jwt/config/SecurityConfig.java
@@ -1,14 +1,14 @@
 package com.yuyeon.jwt.config;
 
 import com.yuyeon.jwt.config.jwt.JwtAuthenticationFilter;
-import com.yuyeon.jwt.filter.MyFilter3;
+import com.yuyeon.jwt.config.jwt.JwtAuthorizationFilter;
+import com.yuyeon.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -17,12 +17,13 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CorsFilter corsFilter;
+    private final UserRepository userRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         //Security 필터가 동작하기 전에 걸어야한다.
-        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
+        //http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
         http.csrf().disable();
 
         //세션 사용하지 않겠다.
@@ -33,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic().disable() // basic 방식 안쓸꺼다.(Bearer 방식 사용할 것이다.)
                 .addFilter(new JwtAuthenticationFilter(
                         authenticationManager())) //AuthenticationManager 를 던져줘야함.
+                .addFilter(new JwtAuthorizationFilter(
+                        authenticationManager(), userRepository)) //AuthenticationManager 를 던져줘야함.
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
